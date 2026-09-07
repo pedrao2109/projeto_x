f106cb82f9d64368523e9d0a8d6624f66d60623f
Pedro <pedroguilhermeferraridegoes@gmail.com>
2026-09-07 20:18:53 -0300

    index

---
+++ projeto y.py
@@ -0,0 +1,9 @@
+gasolina = float(input("Quanto de gasolina foi gasto esse mês? "))
+cartao = float(input("Quanto de dinheiro foi gasto no credito esse mês? "))
+ganhos_totais = float(input("Quais foram os ganhos totais esse mês? "))
+
+#garoto de programa
+
+resultado_final = (ganhos_totais-cartao-gasolina)
+
+print(resultado_final)
